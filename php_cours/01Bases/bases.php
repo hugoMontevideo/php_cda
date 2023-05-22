1459723701fb4503deb874df460b6cce8ccd962d
hugoMontevideo <machadopereira.hugo@gmail.com>
2023-05-22 17:22:14 +0200

    if else

--- php_cours/01Bases/bases.php
+++ php_cours/01Bases/bases.php
@@ -1,18 +1,16 @@
 <?php
 
-$varA = 1;    // integer
-$varB = '1';  // string
 
-if ($varA == $varB) { // avec le double == on compare uniquement la valeur
-    echo "$varA est égal à $varB en valeur <br>";
-}
+$langue = 'chinois';
 
-if ($varA === $varB) {  // avec le triple === on compare la valeur et le type
-    echo 'Les deux variables sont égales en valeur ET en type <br>';
-} else {
-    echo 'Les deux variables sont différentes en valeur OU en type <br>';
+if ($langue == 'français') { // avec le double == on compare uniquement la valeur
+    echo 'bonjour';
+}elseif($langue == 'espagnol'){
+    echo 'hola';
+}else{
+    echo 'je ne sais pas dire bonjour dans votre langue';
 }
 
 ?>
-<p>hello</p>
+
 
